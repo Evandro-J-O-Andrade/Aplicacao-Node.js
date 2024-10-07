309ca4f9db0255ec7e39a9c0d8289627b941d9f9
Evandro Andrade <evandro_j.o.a@hotmail.com>
2024-10-07 06:49:55 -0300

    up
    Up

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const app =express()
+app.use(express.json())
+const users = []
+
+app.post('/usuarios', (req , res) => {
+
+   users.push(req.body)
+
+   res.status(201).json(req.body)
+
+})
+
+app.get('/usuarios', (req, res)=>{
+    res.status(200).json(users)
+
+})
+
+app.listen(3000)
+
+/*
+ Criando nossa API de Usuarios
+
+ -Criando usuarios
+ -Listando dosos os usuarios
+ -Editar um usuario
+ -Deletando usuarios
+
+*/
+ 
