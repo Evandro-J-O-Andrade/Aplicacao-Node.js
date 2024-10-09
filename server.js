b4902d49ee84a1df61f1a14ac19a6b0819697a79
Evandro Andrade <evandro_j.o.a@hotmail.com>
2024-10-09 06:03:20 -0300

    Cod

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app =express()
 app.use(express.json())
 const users = []
 
-app.post('/usuarios', (req , res) => {
+app.post('/usuarios', async (req , res) => {
 
+  await  prisma.user.create({
+    where:{
+
+
+    },
+        data: {
+          name: req.body.name,
+          age: req.body.age,  // Certifique-se de que 'idade' seja do tipo correto, possivelmente um número
+          email: req.body.email
+          
+        }
+      })
+      
    users.push(req.body)
 
    res.status(201).json(req.body)
 
 })
 
-app.get('/usuarios', (req, res)=>{
+app.get('/usuarios', async (req, res)=>{
+   
+    const users = await prisma.user.findMany
     res.status(200).json(users)
-
 })
 
 app.listen(3000)
